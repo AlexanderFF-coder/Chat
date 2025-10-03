0fda060ed9532c633631781b4a9ba82f557fd55d
Daniel <Da@gmail.com>
2025-10-02 20:18:31 -0600

    Visualizacion de chats y mandar mensajes a BD

--- Chat_Interfaces/Chat.Designer.cs
+++ Chat_Interfaces/Chat.Designer.cs
@@ -34,21 +34,24 @@
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(43, 12);
+            this.textBox1.Location = new System.Drawing.Point(57, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(523, 358);
+            this.textBox2.Location = new System.Drawing.Point(563, 507);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(290, 22);
             this.textBox2.TabIndex = 1;
             // 
             // contextMenuStrip1
@@ -62,9 +65,10 @@
             this.listBox1.Font = new System.Drawing.Font("Niagara Solid", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 38;
-            this.listBox1.Location = new System.Drawing.Point(1, 56);
+            this.listBox1.Location = new System.Drawing.Point(1, 69);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(397, 384);
+            this.listBox1.Size = new System.Drawing.Size(528, 460);
             this.listBox1.TabIndex = 3;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -72,22 +76,37 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(159, 12);
+            this.label1.Location = new System.Drawing.Point(212, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(22, 24);
             this.label1.TabIndex = 4;
             this.label1.Text = "+";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(911, 504);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "+";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // Chat
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Chat";
             this.Text = "Chat";
             this.Load += new System.EventHandler(this.Chat_Load);
@@ -104,5 +123,6 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Chat_Interfaces/Chat.cs
+++ Chat_Interfaces/Chat.cs
@@ -20,17 +20,7 @@ namespace Chat_Interfaces
         public Chat()
         {
             InitializeComponent();
-            conexion = new MySqlConnection("Server=localhost;Port=3306;Database=test;Uid=Alex;Pwd=12345");
-            conexion.Open();
-            //LLena el listbox con los grupos que alla en la base de datos (probado y funcional)
-            comando = new MySqlCommand("SELECT Nombre_grupo FROM  grupo", conexion);
-            leer = comando.ExecuteReader();
-            while (leer.Read())
-            {
-                listBox1.Items.Add(leer["Nombre_grupo"].ToString());
-                //Mostrar linea en medio pero no esta activa o puede presionarse en el programa
-                listBox1.Items.Add("--------------------------------------------------");
-            }
+            listBox1.Items.Clear();          
         }
 
         private void Chat_Load(object sender, EventArgs e)
@@ -41,16 +31,30 @@ namespace Chat_Interfaces
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             //Buscar chat en listbox y base de datos y selecionar el que mas se parezca y oculta lo demas (no funciona) 
-            string chec =textBox1.Text;
-            for (int i=0;i<listBox1.Items.Count;i++)
-            {
-                if (listBox1.Items[i].ToString().Contains(chec))
+            string chec=textBox1.Text;
+            //Si no es nada llena con todos los chats
+            listBox1.Items.Clear();
+
+                //LLena el listbox con los grupos que alla en la base de datos (probado y funcional)
+                comando=new MySqlCommand("SELECT Nombre_grupo FROM  grupo",conexion);
+                leer=comando.ExecuteReader();
+                while(leer.Read())
                 {
-                    listBox1.SetSelected(i,true);
+                    listBox1.Items.Add(leer["Nombre_grupo"].ToString());
+                    //Mostrar linea en medio pero no esta activa o puede presionarse en el programa
+                    listBox1.Items.Add("--------------------------------------------------");
                 }
-                else
+            leer.Close();
+            comando.Dispose();
+            for(int i=0;i<listBox1.Items.Count;i++)
+            {
+                //Si esta esconde los elementos que no son
+                //Convierte el texto a un string y en el indice checa si son iguales
+                // StringComparison.CurrentCultureIgnoreCase se encarg de comporar sin importar si es mayus o minus si es menor no son iguales
+                if(listBox1.Items[i].ToString().IndexOf(chec,StringComparison.CurrentCultureIgnoreCase)<0)
                 {
-                    listBox1.SetSelected(i,false);
+                    listBox1.Items.RemoveAt(i);
+                    i--;
                 }
             }
         }
@@ -62,7 +66,7 @@ namespace Chat_Interfaces
 
         private void label1_Click(object sender, EventArgs e)
         {
-            Crea_grupo crea= new Crea_grupo();
+            Crea_grupo crea=new Crea_grupo();
             conexion.Close();
             crea.Show();
             this.Hide();
@@ -70,16 +74,50 @@ namespace Chat_Interfaces
 
         private void Chat_VisibleChanged(object sender, EventArgs e)
         {
-            conexion = new MySqlConnection("Server=localhost;Port=3306;Database=test;Uid=Alex;Pwd=12345");
+            //Cuando es visible lee chat
+            conexion=new MySqlConnection("Server=localhost;Port=3306;Database=test;Uid=Alex;Pwd=12345");
             conexion.Open();
-            comando = new MySqlCommand("SELECT Nombre_grupo FROM  grupo", conexion);
-            leer = comando.ExecuteReader();
-            while (leer.Read())
+            comando=new MySqlCommand("SELECT Nombre_grupo FROM  grupo", conexion);
+            leer=comando.ExecuteReader();
+            while(leer.Read())
             {
                 listBox1.Items.Add(leer["Nombre_grupo"].ToString());
                 //Mostrar linea en medio pero no esta activa o puede presionarse en el programa
                 listBox1.Items.Add("--------------------------------------------------");
             }
+            leer.Close();
+            comando.Dispose();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            int id=0;
+            //Pasa los mensajes a la base de datos
+            conexion=new MySqlConnection("Server=localhost;Port=3306;Database=test;Uid=Alex;Pwd=12345");
+            conexion.Open();
+            //Obtiene el nombre del grupo y la clave del grupo
+            string grupo=listBox1.SelectedItem.ToString();
+            comando=new MySqlCommand("SELECT id FROM grupo WHERE Nombre_grupo=@nom",conexion);
+            comando.Parameters.AddWithValue("@nom",grupo);
+            leer=comando.ExecuteReader();
+            while (leer.Read())
+            {
+                id=(int)leer["id"];
+            }
+            leer.Close();
+            comando.Dispose();
+            if(id==0)
+            {
+                return;
+            }
+            //Inserta el mensaje en la base de datos
+            comando = new MySqlCommand("INSERT INTO mensajes (Id_grupo,contenido) \r\nvalues(@id,@cont) ;", conexion);
+            comando.Parameters.AddWithValue("@id",id);
+            comando.Parameters.AddWithValue("@cont",textBox2.Text);
+            comando.ExecuteNonQuery();
+            comando.Dispose();
+            textBox2.Clear();
+            conexion.Close();
         }
     }
 }
